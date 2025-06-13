25f585c54e44fa766b043e3c94115d00269f0029
Ogün EKİZ <ogunekiz@hotmail.com>
2025-06-13 12:22:49 +0300

    AutoMapper kurulumu

--- CleanArchitecture.Persistence/Services/CarService.cs
+++ CleanArchitecture.Persistence/Services/CarService.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+﻿using AutoMapper;
+using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
 using CleanArchitecture.Application.Services;
 using CleanArchitecture.Domain.Entities;
 using CleanArchitecture.Persistence.Context;
@@ -8,21 +9,18 @@ namespace CleanArchitecture.Persistence.Services;
 public sealed class CarService : ICarService
 {
     private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
 
-    public CarService(AppDbContext context)
+    public CarService(AppDbContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     public async Task CreateAsync(CreateCarCommand request, CancellationToken cancellationToken)
     {
-        Car car = new()
-        {
-            Name = request.Name,
-            Model = request.Model,
-            EnginePower = request.EnginePower
-        };
 
+        Car car = _mapper.Map<Car>(request);
         await _context.Set<Car>().AddAsync(car, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
     }
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICarService, CarService>();
 
+builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistence.AssemblyReference).Assembly);
+
 string connectionString = builder.Configuration.GetConnectionString("SqlServer");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
